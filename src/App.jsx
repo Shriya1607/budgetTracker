512ca803d7f317435497145755f1d9cdda68821c
Shriya1607 <guptashriya03@gmail.com>
2023-10-19 18:42:37 +0530

    Unstyled Form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Select, NumberInput, Button } from "@mantine/core";
+import { DateInput } from "@mantine/dates";
+
+
 
 const App = () => {
   return (
-    <div>Hello World!</div>
-  )
+    <>
+      <Select
+        label="Type"
+        placeholder="Pick a Type"
+        data={["Income", "Expense"]}
+      />
+      <Select
+        label="Category"
+        placeholder="Pick a Category"
+        data={[
+          "Salary",
+          "Investment",
+          "Savings",
+          "Rent",
+          "Groceries",
+          "Transportation",
+        ]}
+      />
+      <NumberInput
+        label="Amount"
+        description="Enter Amount"
+        placeholder="Amount"
+      />
+      <DateInput label="Date input" placeholder="Date input" />
+      <Button variant="filled" color="cyan">
+        Button
+      </Button>
+    </>
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider >
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
